b9aed23739a90bba8f1cd14d64f8ffa25932f8a6
Aamir Husain <aamir300@gmail.com>
2018-04-09 20:21:05 -0500

    Rename project, add pragma values to main.c

--- hw1/hw1_code.X/nbproject/Package-default.bash
+++ hw1/hw1_code.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/code.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=code.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=code.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/hw1_code.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=hw1_code.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=hw1code.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/code.x/bin
+makeDirectory ${TMPDIR}/hw1code.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/code.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/hw1code.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/code.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/hw1code.x.tar *
 checkReturnCode
 
 # Cleanup
