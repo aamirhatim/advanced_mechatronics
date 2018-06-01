c075b518818856b10bd06b40d27a475c138130d9
Aamir Husain <aamir300@gmail.com>
2018-06-01 16:31:20 -0500

    Add copycat app for hw 15, add root for Greens app

---
+++ Copycat/app/src/main/java/com/aamirhatim/copycat/MainActivity.java
@@ -0,0 +1,43 @@
+package com.aamirhatim.copycat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar sendNum;
+    TextView numVal;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendNum = (SeekBar) findViewById(R.id.seek1);
+        numVal = (TextView) findViewById(R.id.textView01);
+    }
+
+    private void setVal() {
+        sendNum.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int valChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                valChanged = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
